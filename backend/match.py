34998fe05bb0613ea48e81e6bb2e263fa1eedd48
emilyliang3 <59712728+emilyliang3@users.noreply.github.com>
2022-01-22 15:37:30 -0800

    third match commit

--- backend/match.py
+++ backend/match.py
@@ -1,7 +1,9 @@
 import json
 
 global_classes = ["MATH 32B" , "CS 31" , "PHYSICS 1A", "ENGR 96A", "CS 32", "CS 33"]
-global_pronouns = ["she/her", "he/him", "they/them", "she/they", "he/they", "they/she", "they/he", "he/she/they", "other"]
+global_pronouns = ["he/him", "he/him", "they/them", "she/they", "he/they", "he/him", "they/he", "he/she/they", "other"]
+databaseOfLists = []
+
 
 # Python3 code to remove whitespace
 def remove(string):
@@ -29,45 +31,51 @@ def getBlurb(json):
     return json.blurb
 
 #match functions
-def match_classes(class_list):
-    index = []
+def match_classes(class_list, database_list):
     for c in class_list:
-        for gc in global_classes:
-            if remove(c).upper() == remove(gc).upper():
-                index.append(global_classes.index(gc))
-    return index
+        for d in database_list:
+            if remove(c).upper() == remove(d).upper():
+                return True
+    return False
 
 #if json.pronoun== "other" the string passed in should be pronounOther
-def match_pronouns(string):
-    index = []
-    for p in global_pronouns:
-        if remove(string).lower() == remove(p).lower():
-            index.append(global_pronouns.index(p))
-    if len(index) < 1:
-        return 0 #if no match was found
-    return index
+def match_pronouns(string, database_pronoun):
+    if remove(string).lower() == remove(database_pronoun).lower():
+            return True
+    return False
+
+def any_match_pronouns(string, databaseOfLists):
+    for d in databaseOfLists:
+        if remove(string).lower() == d["pronoun"].lower():
+            return True
+    return False
 
 def match(json):
-    class_indices = match_classes(json.classes)
-    pronoun_indices = match_pronouns(json.pronoun)
-    if pronoun_indices == 0:
-        return class_indices
-    matched_indices = []
-    for c in class_indices:
-        for p in pronoun_indices:
-            if c == p:
-                matched_indices.append(c)
-    return matched_indices
+    index = []
+    if not any_match_pronouns(json["pronoun"], databaseOfLists):
+        for i, person in enumerate(databaseOfLists):
+            if match_classes(json["classes"], person["classes"]):
+                index.append(i)
+    else:
+        for i, person in enumerate(databaseOfLists):
+            if match_classes(json["classes"], person["classes"]) and match_pronouns(json["pronoun"], person["pronoun"]):
+                index.append(i)
+    return index
 
 
-    
+data_set = {"classes": ["math"], "pronoun": "they/them"}
+json_one = json.dumps(data_set)
+json_obj = json.loads(json_one)
+data_set_two = {"classes": ["MATH 32B" , "CS 31" , "PHYSICS 1A", "ENGR 96A", "CS 32", "CS 33"], "pronoun": "she/her"}
+json_two = json.dumps(data_set_two)
+json_obj2 = json.loads(json_two)
+data_set_three = {"classes": ["MATH 32B" , "ENGR 96A", "CS 32", "CS 33"], "pronoun": "he/him"}
+json_three = json.dumps(data_set_three)
+json_obj3 = json.loads(json_three)
+databaseOfLists.append(json_obj2)
+databaseOfLists.append(json_obj3)
+print (match(json_obj))
 
-classlist1 = ["cs31","engr 96a", "MATH 32b ", "cs 35l    "," CS 33"]
-test = match_classes(classlist1)
-for t in test:
-    print (t)
-test2 = match_pronouns("he/they")
-print (test2)
 
 
 
