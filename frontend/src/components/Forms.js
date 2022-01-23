0cf7d81ae3783dcc8e1c28c4c5add125d48a3db5
Benson Liu <bensonhliu@gmail.com>
2022-01-22 21:25:13 -0800

    Betterfirebase (#13)
    * fixed header
    
    * username checking works
    
    * cleaner
    
    Co-authored-by: Amanda Wang <theamandawang@gmail.com>

--- frontend/src/components/Forms.js
+++ frontend/src/components/Forms.js
@@ -1,8 +1,7 @@
 import { useForm } from 'react-hook-form';
 import db from '../config';
 import { useState } from 'react';
-import { ref, set, push, child } from "firebase/database";
-
+import { ref, set, get, child } from "firebase/database";
 const pronouns = [
   { value: 'he-him', text: 'he/him' },
   { value: 'he-they', text: 'he/they' },
@@ -16,38 +15,51 @@ const pronouns = [
 
 
 function Forms() {
-  const { register, handleSubmit, reset, clearErrors, watch } = useForm();
+  const { register, handleSubmit, watch } = useForm();
 
   const onSubmit = data => {
     update(data.classes);
+    console.log(data.classes);
 
   }  // put function that calls backend
 
   const watchNumClass = watch('numClass');
-  const watchPronoun = watch('pronoun');
 
   function classNums() {
     return [...Array(parseInt(watchNumClass || 0)).keys()];
   }
 
 let [name, setName] = useState('');
+let [user, setUser] = useState('');
+let [pass, setPass] = useState('');
 let [pronounsV, setPronounsV] = useState('he/him');
 let [contact, setContact] = useState('');
 let [otherPronoun, setOtherPronoun] = useState('');
 let [blurb, setBlurb] = useState('');
-  const update = (classes) => {
+  async function update(classes) {
+    let boolCheckUser = false;
+    let db_snapshot = await get(child(ref(db), 'users/'));
+      db_snapshot.forEach(code_snap =>{
+        if(code_snap.key === user){
+          alert('Username taken!');
+          boolCheckUser = true;
+          return;
+        }
+      });
+    if(boolCheckUser) return;
     let finalPronoun = pronounsV === 'other' ? otherPronoun : pronounsV
     if(finalPronoun === 'he/him') finalPronoun = 'he-him';
-    push(child(ref(db), 'users/'), {
+    set(child(ref(db), 'users/' + user), {
       name: name,
       pronouns: finalPronoun,
       contact: contact,
       blurb: blurb,
-      classes: classes
+      classes: classes,
+      password: pass,
     });
   }
   function testOther () {
-    if (pronounsV == 'other') {
+    if (pronounsV === 'other') {
       return (
         <div>
           <div className='row'>
@@ -69,9 +81,6 @@ let [blurb, setBlurb] = useState('');
 
   return (
     <div>
-
-      <h1>Form</h1>
-
       <form onSubmit={handleSubmit(onSubmit)}>
 
         <div className='row'>
@@ -82,6 +91,28 @@ let [blurb, setBlurb] = useState('');
           value={name}
           onChange={(e) => setName(e.target.value)}
           placeholder='First and Last Name'
+        /></div></div>
+        <br />
+
+        <div className='row'>
+        <div className='left-column'>Username</div>
+        <div className='right-column'>
+          <input
+          type='text'
+          value={user}
+          onChange={(e) => setUser(e.target.value)}
+          placeholder='Username'
+        /></div></div>
+        <br />
+
+        <div className='row'>
+        <div className='left-column'>Password</div>
+        <div className='right-column'>
+          <input
+          type='text'
+          value={pass}
+          onChange={(e) => setPass(e.target.value)}
+          placeholder='Password'
         /></div></div>
         <br />
 
@@ -137,6 +168,7 @@ let [blurb, setBlurb] = useState('');
             <div className='right-column'><input
               type='text'
               placeholder=''
+              {...register(`classes[${i}]`)}
             /></div></div>
             <br />
           </div>
