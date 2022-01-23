3aa22e07996fe8e86d30e9723836f57b8eb2b660
Amanda Wang <52474776+theamandawang@users.noreply.github.com>
2022-01-22 17:34:23 -0800

    data is added to firebase (#8)
    Co-authored-by: Benson Liu <bensonhliu@gmail.com>

---
+++ frontend/src/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+const db = initializeApp({
+  apiKey: "AIzaSyAZpxYMmrt52ewsblpgdBuYfswUx6wWHqQ",
+  authDomain: "qwerty-f9aa1.firebaseapp.com",
+  databaseURL: "https://qwerty-f9aa1-default-rtdb.firebaseio.com",
+  projectId: "qwerty-f9aa1",
+  storageBucket: "qwerty-f9aa1.appspot.com",
+  messagingSenderId: "38457080982",
+  appId: "1:38457080982:web:cbfbc0ec2e85ed3fc656b7",
+  measurementId: "G-Y6Q04Q0MS5"
+  });
+
+  
+
+const database = getDatabase()
+export default database;
