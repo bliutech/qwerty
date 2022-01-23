524e126f3c9c6d1370e0e1420b11eb5d743ad29e
bliutech <bensonhliu@gmail.com>
2022-01-22 18:08:35 -0800

    updated stuff

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,16 @@
+import {Link} from 'react-router-dom';
+import classes from './NavBar.module.css';
+
+export default function NavBar(){
+    return(
+        <header className={classes.header}>
+            <h1 className={classes.logo}> <Link to='/'>Home</Link></h1>
+            <nav>
+                <ul>
+                <li><Link to='/register'>Register</Link></li>
+                    <li><Link to='/login'>Login</Link></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
