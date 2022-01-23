f9b16990540770e0618c72778993986274a33999
Kuan Heng Lin <Jordan2781278321@gmail.com>
2022-01-22 18:19:17 -0800

    Added results page/component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import RegisterMod from './components/RegisterMod.js'
 import LoginMod from './components/LoginMod.js'
 import Forms from './components/Forms.js'
+import ResultMod from './components/ResultMod.js'
 
 function App() {
   return (
     <div className='general'>
       {/* <RegisterMod /> */}
       {/* <LoginMod /> */}
-      <Forms />
+      {/* <Forms /> */}
+      <ResultMod />
     </div>
   );
 }
